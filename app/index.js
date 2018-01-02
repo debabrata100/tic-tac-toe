eeedfa685dbf21ead6759e2752057ef965df1467
Deb <m.debabrata100@gmail.com>
2018-01-02 19:08:42 +0530

    Preparing playing board dynamically with html table

--- app/index.js
+++ app/index.js
@@ -1,10 +1,39 @@
 (function(window){
 
-  let _init = ({container = ''})=>{
+  let _init = ({container = '',cellHeight = 50, cellWidth = 50})=>{
     let gameBody = document.getElementById(container);
-    if(gameBody === undefined)
-      gameBody = document.getElementsByTagName("body")[0];
-      console.log("Game Initialised");
+      if(gameBody === undefined)
+        gameBody = document.getElementsByTagName("body")[0];
+
+          let tbl     = document.createElement("table");
+          let tblBody = document.createElement("tbody");
+          for (let i = 0; i < 3; i++) {
+              var row = document.createElement("tr");
+              for (let j = 0; j < 3; j++) {
+               let cell = document.createElement("td");
+                cell.setAttribute("width",cellHeight+"px");
+                cell.setAttribute("height",cellWidth+"px");
+
+                //setting styles for table cells
+                cell.style.textAlign = "center";
+                cell.style.cursor = "pointer";
+                cell.style.fontSize = (cellHeight*0.4) + 'px';
+
+                row.appendChild(cell);
+              }
+              tblBody.appendChild(row);
+          }
+
+        tbl.appendChild(tblBody);
+        gameBody.appendChild(tbl);
+        tbl.setAttribute("border", "1");
+
+        // setting styles for table
+        tbl.style.borderCollapse = 'collapse';
+        tbl.style.border = '2px solid #000';
+        tbl.style.height = "auto";
+        tbl.style.width = "auto";
+        tbl.style.margin = "0 auto";
   }
 
 window.Game = {
